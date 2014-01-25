067b0ea9f9048a57ca1c445de7104b08fb4481ec
Olivier <nguyenolive@gmail.com>
2014-01-24 21:52:08 -0500

    Made changes to use the i2c library found

--- src/acs.cpp
+++ src/acs.cpp
@@ -1,18 +1,10 @@
-// TODO: Fix for Beaglebone black
+// TODO: Change i2c_read() & i2c_write() to follow the library
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <stropts.h>
-#include <stdio.h>
+#include "../include/i2c.h"
+#include <string.h>
 #include <iostream>
-#include <math.h>
 
-using namespace std;
-
+// Define device address
 #define  ADXL345_ADDRESS (0xA6 >> 1)
 #define ADXL345_REGISTER_XLSB (0x32)
 #define ADXL_REGISTER_PWRCTL (0x2D)
@@ -33,89 +25,42 @@ int accelerometer_data[3];
 int gyro_data[3];
 int magnetometer_data[3];
 
-char c;
+using namespace std;
+void init_adxl345();
+void init_hmc5843();
+void init_itg3200();
 
-void setup() {
-  Wire.begin();
-  Serial.begin(9600);
+void read_adxl345();
+void read_hmc5843();
+void read_itg3200();
 
-  for(int i = 0; i < 3; ++i) {
-    accelerometer_data[i] = magnetometer_data[i] = gyro_data[i] = 0;
-  }
-  
-  init_adxl345();
-  init_hmc5843();
-  init_itg3200();
+int main(){
+  // Instantiate the I2c Object
+  myI2C *i2cptr = new myI2C();
+          
+  int value = 10;
+          
+  i2cptr->Send_I2C_Byte(DEVICE_ADDR,CTRL_REG1,value); // Example send data
+          
+  value = i2cptr->Read_I2C_Byte(DEVICE_ADDR, STATUS_REG); // Read single byte
+          
+  i2cptr->Read_Multi_Byte(DEVICE_ADDR, 0x80 | STATUS_REG);  // Example read multiple data
+  // Output data will be stored in i2cptr->I2C_RD_Buf. Check i2c.cpp for more details
+        
+  return 0;
 }
-
-void loop() {
-   read_adxl345();
-
-   printf("ACCEL: ");
-   printf(accelerometer_data[0]);
-   printf("\t");
-   printf(accelerometer_data[1]);
-   printf("\t");
-   printf(accelerometer_data[2]);
-   printf("\t");
-
-   read_hmc5843();
-
-   printf("MAG: ");
-   printf(magnetometer_data[0]);
-   printf(",");
-   printf(magnetometer_data[1]);
-   printf(",");
-   printf(magnetometer_data[2]);
-   printf("\t");
-
-   read_itg3200();
-
-   printf("GYRO: ");
-   printf(gyro_data[0]);
-   printf("\t");
-   printf(gyro_data[1]);
-   printf("\t");
-   printf(gyro_data[2]);
-   printf("\n");
-
-   delay(100);
-}
-
-void i2c_write(int address, byte reg, byte data) {
-  Wire.beginTransmission(address);
-  Wire.write(reg);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
-void i2c_read(int address, byte reg, int count, byte* data) {
- int i = 0;
-
- Wire.beginTransmission(address);
- Wire.write(reg);
- Wire.endTransmission();
- Wire.beginTransmission(address);
- Wire.requestFrom(address,count);
- while(Wire.available()){
-   c = Wire.read();
-   data[i] = c;
-   i++;
- }
- Wire.endTransmission();
-} 
 
 void init_adxl345() {
-  byte data = 0;
+  unsigned int data = 0;
 
   i2c_write(ADXL345_ADDRESS, ADXL_REGISTER_PWRCTL, ADXL_PWRCTL_MEASURE);
 
   i2c_read(ADXL345_ADDRESS, ADXL_REGISTER_PWRCTL, 1, &data);
-  printfln((unsigned int)data);
+  printf("%d",(unsigned int)data);
 }
 
 void read_adxl345() {
- byte bytes[6];
+ unsigned int bytes[6];
  memset(bytes,0,6);
 
  i2c_read(ADXL345_ADDRESS, ADXL345_REGISTER_XLSB, 6, bytes);
@@ -126,17 +71,17 @@ void read_adxl345() {
 }
 
 void init_itg3200() {
-  byte data = 0;
+  unsigned int data = 0;
 
   i2c_write(ITG3200_ADDRESS, ITG3200_REGISTER_DLPF_FS, ITG3200_FULLSCALE | ITG3200_42HZ);
 
   i2c_read(ITG3200_ADDRESS, ITG3200_REGISTER_DLPF_FS, 1, &data);
 
-  printfln((unsigned int)data);
+  printf("%d\n",(unsigned int)data);
 }
 
 void read_itg3200() {
-  byte bytes[6];
+  unsigned int bytes[6];
   memset(bytes,0,6);
 
   i2c_read(ITG3200_ADDRESS, ITG3200_REGISTER_XMSB, 6, bytes);
@@ -146,16 +91,16 @@ void read_itg3200() {
 }
 
 void init_hmc5843() {
-  byte data = 0;
+  unsigned int data = 0;
   
   i2c_write(HMC5843_ADDRESS, HMC5843_REGISTER_MEASMODE, HMC5843_MEASMODE_CONT);
 
   i2c_read(HMC5843_ADDRESS, HMC5843_REGISTER_MEASMODE, 1, &data);
-  printfln((unsigned int)data);
+  printf("%d",(unsigned int)data);
 }
 
 void read_hmc5843() {
- byte bytes[6];
+ unsigned int bytes[6];
  memset(bytes,0,6);
 
  i2c_read(HMC5843_ADDRESS, HMC5843_REGISTER_XMSB, 6, bytes);
@@ -163,13 +108,4 @@ void read_hmc5843() {
  for (int i=0;i<3;++i) {
  magnetometer_data[i] = (int)bytes[2*i + 1] + (((int)bytes[2*i]) << 8);
  }
-}
-
-int main()
-{
-
-  setup();
-  loop();
-
-  return 0;
 }
