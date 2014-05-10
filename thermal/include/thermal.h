ee808a8f0fc88b67c99636c352dc8bb330d6e86c
Olivier <nguyenolive@gmail.com>
2014-05-09 23:01:27 -0400

    Updated thermistor pins

--- thermal/include/thermal.h
+++ thermal/include/thermal.h
@@ -1,6 +1,14 @@
 #ifndef __THERMAL_H__
 #define __THERMAL_H__
 
+#define TH_BAT_1 "P8_13" // GPIO_23
+#define TH_BAT_2 "P8_15"// GPIO_47
+#define TH_PWR_3 "P8_17" // GPIO_27
+#define TH_SPF "P8_14" // GPIO_26
+#define TH_SPB "P8_16" // GPIO_46
+#define TH_SPR "P8_18" // GPIO_65
+#define TH_SPL "P8_26" // GPIO_61
+
 typedef enum thermal_sensors_e {
   THERMAL_SENSOR_MECH1 = 0;
   THERMAL_SENSOR_MECH2 = 1;
--- watchdog/include/watchdog.h
+++ watchdog/include/watchdog.h
@@ -1,6 +1,8 @@
 #ifndef __WATCHDOG_H__
 #define __WATCHDOG_H__
 
+#define WATCHDOG_INPUT "P9_21" // GPIO_3
+
 void watchdog_force_cpu_rest(void);
 void watchdog_enable(uint32_t seconds);
 void watchdog_disable(void);
